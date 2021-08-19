897ab2e0a131e39ef0828726127cfaa786b56701
atmygml <43545518+atmygml@users.noreply.github.com>
2021-08-19 12:17:45 +0800

    Update check_for_positive_numbers.py

--- check_for_positive_numbers.py
+++ check_for_positive_numbers.py
@@ -1,34 +1,42 @@
 import string
 
-number_as_string = string.digits
+def check_num():
 
-number_input = input("Please enter a positive number.\n\n") 
+  number_as_string = string.digits
 
-valid_input = False
+  number_input = input("\nPlease enter a positive number.\n\n") 
 
-while valid_input == False:
+  valid_input = False
+
+  while valid_input == False:
+      
+    if number_input == "":
+      print("\nYou have entered an invalid input.\n")
+      number_input = input("Please enter a positive number again.\n\n")
+      valid_input = False
     
-  if number_input == "":
-    print("\nYou have entered an invalid input.\n")
-    number_input = input("Please enter a positive number again.\n\n")
-    valid_input = False
-  
-  elif number_input[0] == "0":
-    print("\nYou have entered an invalid input.\n")
-    number_input = input("Please enter a positive number again.\n\n")
-    valid_input = False
+    elif number_input[0] == "0":
+      print("\nYou have entered an invalid input.\n")
+      number_input = input("Please enter a positive number again.\n\n")
+      valid_input = False
 
-  else:
-    for i in range(len(number_input)):
-      if number_input[i] not in number_as_string:
-        print("\nYou have entered an invalid input.\n")
-        number_input = input("Please enter a positive number again.\n\n")
-        valid_input = False
-        break
-      if i == (len(number_input)-1):
-        print("\nYou have entered a valid input.\n")
-        number_entered = int(number_input)
-        print(f"\nThe number you have entered is {number_entered}.\n")
-        print("You may proceed.\n")
-        print("Program ends.\n")
-        valid_input = True
+    else:
+      for i in range(len(number_input)):
+        if number_input[i] not in number_as_string:
+          print("\nYou have entered an invalid input.\n")
+          number_input = input("Please enter a positive number again.\n\n")
+          valid_input = False
+          break
+        if i == (len(number_input)-1):
+          print("\nYou have entered a valid input.\n")
+          number_entered = int(number_input)
+          #print(f"\nThe number you have entered is {number_entered}.\n")
+          print("You may proceed.\n")
+          #print("Program ends.\n")
+          valid_input = True
+          return number_entered
+
+number_checked = check_num()
+print(f"\nThe number you have entered is {number_checked}.\n")
+print("\nProgram ends.\n")
+
